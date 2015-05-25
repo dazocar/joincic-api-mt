f11fb07b2f376c49219109bc6fe831fda065a4f0
Daniel Azocar <daniel.azocar18@gmail.com>
2015-05-25 15:13:47 -0430

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoincicApiMt::Application.config.secret_key_base = 'ceaf83285e1ef364682a1c7e675816a483c9bacb73ff731700d63adeae807793d69b86b2850aa1560789801abfc1ffb013c36d54d4d0fa8f6f80dda4ed2b8e2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JoincicApiMt::Application.config.session_store :cookie_store, key: '_joincic-api-mt_session'
